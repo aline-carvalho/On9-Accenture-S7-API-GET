55413bd00787e2cef1ce92e15e328bc96d5713e7
aline-carvalho <alinecarvalhomendes03@gmail.com>
2020-09-24 22:29:24 -0300

    Exercicios finalizados  API GET

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require('express');
 const app = express();
 const porta = 3000;
 const estudantes = require('./estudantes.json')
+const mulheres = require('./mulheresTec.json')
 
 app.use(express.json());
 
 
-app.get('/estudantes', function(requisicao, resposta){
-  resposta.json(estudantes);
+app.get('/mulheres', function(requisicao, resposta){
+  resposta.json(mulheres);
 });
 
+app.get('/mulheres/:id', function(requisicao, resposta){
+  console.log(requisicao.params.id)
+  const divasTecnologia = mulheres.find(mulher => mulher.id === parseInt(requisicao.params.id))
+  console.log(divasTecnologia)
+  resposta.json(divasTecnologia);
+});
+
+
 app.listen(porta, function(){
+
   console.log("Servidor rodando")
 });
